2165768be2177c0b709e1c8dcef051dfecbb2703
Jenn Roman <jeanelyn.roman@snhu.edu>
2025-04-12 23:37:58 -0700

    Module 6 completed baseline

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 
 
@@ -31,9 +32,17 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Enable CORS
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+})
 
 // wire-up routes to controllers 
 app.use('/', indexRouter);
--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -8,13 +8,12 @@ const tripsList = async (req, res) => {
     .find({}) // No filter, return all records
     .exec();
 
-  
   console.log(q);
 
   if (!q) {
     return res
       .status(404)
-      .json(err);
+      .json({ message: 'No trips found' });
   } else {
     return res
       .status(200)
@@ -22,26 +21,84 @@ const tripsList = async (req, res) => {
   }
 };
 
+// GET: /trips/:tripCode - find a trip by code
 const tripsFindByCode = async (req, res) => {
   const q = await Model
-    .find({'code' : req.params.tripCode }) // Return single record
+    .find({ 'code': req.params.tripCode }) // Return single record
     .exec();
 
-  
   console.log(q);
 
   if (!q) {
     return res
       .status(404)
-      .json(err);
-  } else {  //Return resulting trip list
+      .json({ message: 'Trip not found' });
+  } else {
     return res
       .status(200)
       .json(q);
   }
 };
 
+// POST: /trips - Add a new Trip
+const tripsAddTrip = async (req, res) => {
+  const newTrip = new Trip({
+    code: req.body.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description
+  });
+
+  const q = await newTrip.save();
+
+  if (!q) {
+    return res
+      .status(400)
+      .json({ message: 'Failed to create trip' });
+  } else {
+    return res
+      .status(201) // Created
+      .json(q);
+  }
+};
+
+// PUT: /trips/:tripCode - Update an existing Trip
+const tripsUpdateTrip = async (req, res) => {
+
+  const q = await Model.findOneAndUpdate(
+    { code: req.params.tripCode },
+    {
+      code: req.body.code,
+      name: req.body.name,
+      length: req.body.length,
+      start: req.body.start,
+      resort: req.body.resort,
+      perPerson: req.body.perPerson,
+      image: req.body.image,
+      description: req.body.description
+    },
+    { new: true, runValidators: true }
+  ).exec();
+
+  if (!q) {
+    return res
+      .status(400)
+      .json(err);
+  } else {
+    return res
+      .status(201)
+      .json(q);
+  }
+};
+
+// Export all methods
 module.exports = {
   tripsList,
-  tripsFindByCode
+  tripsFindByCode,
+  tripsAddTrip,
+  tripsUpdateTrip
 };
